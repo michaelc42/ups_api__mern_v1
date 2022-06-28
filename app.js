10696c7de5421f745e4e62d30b204eab2119dd14
michaelc42 <michaelcasey42@gmail.com>
2022-06-28 15:51:06 -0500

    changed build: webpack added to server scripts

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use('/api/v1/ups', upsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 const path = require("path");
 
