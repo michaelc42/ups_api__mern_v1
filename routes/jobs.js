79d4fc347f125772e13af7d1524ade596dbafecd
michaelc42 <michaelcasey42@gmail.com>
2022-06-29 01:40:08 -0500

    removed unneeded files

--- routes/jobs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const { getAllJobs, getJob, createJob, updateJob, deleteJob,getEveryonesJob } = require('../controllers/jobs')
-
-router.route('/').post(createJob).get(getEveryonesJob)//.get(getAllJobs)
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
-
-module.exports = router
